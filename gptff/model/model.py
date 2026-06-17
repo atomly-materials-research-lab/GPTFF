a1ffea1a2079aa3101bbdfe8ca2717fe0054a940
tllu <lutenglong@iphy.ac.cn>
2026-06-17 08:25:55 +0800

    Add residual interaction blocks

--- gptff/model/model.py
+++ gptff/model/model.py
@@ -107,6 +107,118 @@ class ConvLayer(nn.Module):
         return atom_fea
 
 
+class ThreeBodyEdgeDelta(nn.Module):
+    def __init__(self, atom_fea_len, nbr_fea_len, num_radial, num_angular):
+        super().__init__()
+        self.atom_fea_len = atom_fea_len
+        self.nbr_fea_len = nbr_fea_len
+        self.angle_basis = FourierAngleBasis(num_angular)
+        self.angle_embedding = nn.Linear(self.angle_basis.out_dim, nbr_fea_len, bias=False)
+        self.bond_embedding_k = nn.Linear(num_radial, nbr_fea_len, bias=False)
+        self.bond_embedding_j = nn.Linear(num_radial, nbr_fea_len, bias=False)
+
+        self.sig = nn.Sigmoid()
+        self.swish = nn.SiLU()
+
+        self.W_fea = nn.Linear(3 * atom_fea_len + 2 * nbr_fea_len, nbr_fea_len)
+        self.W_1 = nn.Linear(nbr_fea_len, nbr_fea_len)
+        self.W_2 = nn.Linear(nbr_fea_len, nbr_fea_len)
+
+    def forward(self, atom_fea, edge_ij, graph, triplet_basis_ij, triplet_basis_ik):
+        if graph.triplet_edge_index.numel() == 0:
+            return edge_ij.new_zeros(edge_ij.shape)
+
+        edge_ij_indices = graph.triplet_edge_index[0]
+        edge_ik_indices = graph.triplet_edge_index[1]
+        triple_i_indices = graph.edge_index[0][edge_ij_indices]
+        triple_j_indices = graph.edge_index[1][edge_ij_indices]
+        triple_k_indices = graph.edge_index[1][edge_ik_indices]
+        atom_fea_ik = torch.cat([
+            atom_fea[triple_i_indices],
+            atom_fea[triple_j_indices],
+            atom_fea[triple_k_indices],
+            edge_ij[edge_ij_indices],
+            edge_ij[edge_ik_indices],
+        ], dim=-1)
+
+        atom_fea_ik = self.swish(self.W_fea(atom_fea_ik))
+
+        angles_mat = self.angle_embedding(self.angle_basis(graph.triplet_cosine))
+        bonds_mat_k = self.bond_embedding_k(triplet_basis_ik)
+        bonds_mat_j = self.bond_embedding_j(triplet_basis_ij)
+        atom_fea_ik = (
+            self.sig(self.W_1(atom_fea_ik))
+            * self.swish(self.W_2(atom_fea_ik))
+            * bonds_mat_j
+            * bonds_mat_k
+            * angles_mat
+        )
+
+        edge_delta = edge_ij.new_zeros(edge_ij.shape)
+        return torch.index_add(edge_delta, 0, edge_ij_indices, atom_fea_ik)
+
+
+class AtomFeatureDelta(nn.Module):
+    def __init__(self, atom_fea_len, nbr_fea_len, num_radial):
+        super().__init__()
+        self.atom_fea_len = atom_fea_len
+        self.nbr_fea_len = nbr_fea_len
+        self.fc_full = nn.Linear(2 * atom_fea_len + nbr_fea_len, 2 * atom_fea_len)
+        self.sig = nn.Sigmoid()
+        self.swish = nn.SiLU()
+
+        self.W_r = nn.Linear(num_radial, atom_fea_len, bias=False)
+        self.W_1 = nn.Linear(2 * atom_fea_len, atom_fea_len)
+        self.W_2 = nn.Linear(2 * atom_fea_len, atom_fea_len)
+
+    def forward(self, atom_fea, edge_ij, edge_basis, graph):
+        atom_nbr_fea = torch.cat([
+            atom_fea[graph.edge_index[0]],
+            atom_fea[graph.edge_index[1]],
+            edge_ij,
+        ], dim=-1)
+        atom_gated_fea = self.swish(self.fc_full(atom_nbr_fea))
+
+        atom_msg = self.swish(self.W_1(atom_gated_fea)) * self.sig(self.W_2(atom_gated_fea))
+        atom_msg = atom_msg * self.W_r(edge_basis)
+
+        atom_delta = atom_fea.new_zeros(atom_fea.shape)
+        return torch.index_add(atom_delta, 0, graph.edge_index[0], atom_msg.to(atom_delta.dtype))
+
+
+class InteractionBlock(nn.Module):
+    def __init__(self, atom_fea_len, nbr_fea_len, num_radial, num_angular):
+        super().__init__()
+        self.three_body = ThreeBodyEdgeDelta(
+            atom_fea_len=atom_fea_len,
+            nbr_fea_len=nbr_fea_len,
+            num_radial=num_radial,
+            num_angular=num_angular,
+        )
+        self.edge_update = EdgeUpdate(atom_fea_len, nbr_fea_len, num_radial)
+        self.atom_update = AtomFeatureDelta(atom_fea_len, nbr_fea_len, num_radial)
+        self.triplet_edge_norm = nn.LayerNorm(nbr_fea_len)
+        self.pair_edge_norm = nn.LayerNorm(nbr_fea_len)
+        self.atom_norm = nn.LayerNorm(atom_fea_len)
+
+    def forward(self, atom_fea, edge_ij, graph, edge_basis, triplet_basis_ij, triplet_basis_ik):
+        triplet_delta = self.three_body(
+            atom_fea,
+            edge_ij,
+            graph,
+            triplet_basis_ij,
+            triplet_basis_ik,
+        )
+        edge_ij = self.triplet_edge_norm(edge_ij + triplet_delta)
+
+        pair_delta = self.edge_update(atom_fea, edge_ij, graph, edge_basis)
+        edge_ij = self.pair_edge_norm(edge_ij + pair_delta)
+
+        atom_delta = self.atom_update(atom_fea, edge_ij, edge_basis, graph)
+        atom_fea = self.atom_norm(atom_fea + atom_delta)
+        return atom_fea, edge_ij
+
+
 class AtomWiseReadout(nn.Module):
     def __init__(self, atom_fea_len):
         super().__init__()
@@ -316,19 +428,15 @@ class tModLodaer(nn.Module):
         self.w_eij = nn.Linear(nbr_fea_len* 3, nbr_fea_len)
         self.w_r = nn.Linear(num_radial, nbr_fea_len, bias=False)
 
-        self.convs = nn.ModuleList([ConvLayer(atom_fea_len=atom_fea_len,
-                                    nbr_fea_len=nbr_fea_len,
-                                    num_radial=num_radial)
-                                    for _ in range(n_layers)])
-        
-        self.three = nn.ModuleList([ThreeBody(atom_fea_len=atom_fea_len,
-                                    nbr_fea_len=nbr_fea_len,
-                                    num_radial=num_radial,
-                                    num_angular=num_angular,
-                                    device=self.device) for _ in range(n_layers)])
-
-        self.edge_updates = nn.ModuleList(EdgeUpdate(atom_fea_len, nbr_fea_len, num_radial)
-                                          for _ in range(n_layers))
+        self.interactions = nn.ModuleList([
+            InteractionBlock(
+                atom_fea_len=atom_fea_len,
+                nbr_fea_len=nbr_fea_len,
+                num_radial=num_radial,
+                num_angular=num_angular,
+            )
+            for _ in range(n_layers)
+        ])
 
         self.readout = AtomWiseReadout(atom_fea_len)
 
@@ -353,9 +461,14 @@ class tModLodaer(nn.Module):
 
         edge_ij = self.w_eij(edge_ij) * self.w_r(edge_basis)
         
-        for edge_func, conv, three in zip(self.edge_updates, self.convs, self.three):
-            edge_ij = three(atom_fea, edge_ij, graph, triplet_basis_ij, triplet_basis_ik)
-            edge_ij = edge_ij + edge_func(atom_fea, edge_ij, graph, edge_basis)
-            atom_fea = conv(atom_fea, edge_ij, edge_basis, graph)
+        for interaction in self.interactions:
+            atom_fea, edge_ij = interaction(
+                atom_fea,
+                edge_ij,
+                graph,
+                edge_basis,
+                triplet_basis_ij,
+                triplet_basis_ik,
+            )
 
         return self.readout(atom_fea, graph.atom_batch, graph.num_atoms.shape[0])
---
+++ tests/test_interaction_block.py
@@ -0,0 +1,104 @@
+from types import SimpleNamespace
+
+import torch
+import torch.nn as nn
+from pymatgen.core import Lattice, Structure
+
+from gptff.graph import CrystalGraphBatch, CrystalGraphConverter
+from gptff.model.model import InteractionBlock, tModLodaer
+
+
+def _cfg(n_layers=1):
+    return SimpleNamespace(
+        node_feature_len=8,
+        edge_feature_len=8,
+        n_layers=n_layers,
+        num_radial=8,
+        num_angular=4,
+        radial_cutoff=3.0,
+        angle_cutoff=3.0,
+        cutoff_coeff=5,
+        device="cpu",
+    )
+
+
+def _batch(a_cut=3.0):
+    structure = Structure(
+        Lattice.cubic(3.0),
+        ["Na", "Cl"],
+        [[0.0, 0.0, 0.0], [0.5, 0.5, 0.5]],
+    )
+    graph = CrystalGraphConverter(r_cut=3.0, a_cut=a_cut).convert(structure)
+    return CrystalGraphBatch.from_graphs([graph], ref_energies=[0.0]).with_geometry()
+
+
+def test_non_transformer_model_uses_interaction_blocks():
+    model = tModLodaer(_cfg(n_layers=2))
+
+    assert len(model.interactions) == 2
+    assert isinstance(model.interactions[0], InteractionBlock)
+    assert isinstance(model.interactions[0].triplet_edge_norm, nn.LayerNorm)
+    assert isinstance(model.interactions[0].pair_edge_norm, nn.LayerNorm)
+    assert isinstance(model.interactions[0].atom_norm, nn.LayerNorm)
+
+
+def test_interaction_block_returns_finite_atom_and_edge_features():
+    graph = _batch()
+    model = tModLodaer(_cfg())
+
+    atom_fea = model.atom_embedding(graph.atom_types)
+    edge_basis = model.edge_rbf(graph.edge_lengths)
+    triplet_basis_ij = model.triplet_rbf(graph.triplet_lengths_ij)
+    triplet_basis_ik = model.triplet_rbf(graph.triplet_lengths_ik)
+
+    edge_ij = model.w_b(edge_basis)
+    edge_ij = torch.cat([
+        atom_fea[graph.edge_index[0]],
+        atom_fea[graph.edge_index[1]],
+        edge_ij,
+    ], dim=-1)
+    edge_ij = model.w_eij(edge_ij) * model.w_r(edge_basis)
+
+    atom_out, edge_out = model.interactions[0](
+        atom_fea,
+        edge_ij,
+        graph,
+        edge_basis,
+        triplet_basis_ij,
+        triplet_basis_ik,
+    )
+
+    assert atom_out.shape == atom_fea.shape
+    assert edge_out.shape == edge_ij.shape
+    assert torch.isfinite(atom_out).all()
+    assert torch.isfinite(edge_out).all()
+
+
+def test_three_body_edge_delta_is_zero_without_angle_triplets():
+    graph = _batch(a_cut=1.0)
+    model = tModLodaer(_cfg())
+
+    assert graph.triplet_edge_index.numel() == 0
+
+    atom_fea = model.atom_embedding(graph.atom_types)
+    edge_basis = model.edge_rbf(graph.edge_lengths)
+    edge_ij = model.w_b(edge_basis)
+    edge_ij = torch.cat([
+        atom_fea[graph.edge_index[0]],
+        atom_fea[graph.edge_index[1]],
+        edge_ij,
+    ], dim=-1)
+    edge_ij = model.w_eij(edge_ij) * model.w_r(edge_basis)
+
+    triplet_basis_ij = model.triplet_rbf(graph.triplet_lengths_ij)
+    triplet_basis_ik = model.triplet_rbf(graph.triplet_lengths_ik)
+    delta = model.interactions[0].three_body(
+        atom_fea,
+        edge_ij,
+        graph,
+        triplet_basis_ij,
+        triplet_basis_ik,
+    )
+
+    assert delta.shape == edge_ij.shape
+    assert torch.equal(delta, torch.zeros_like(edge_ij))
